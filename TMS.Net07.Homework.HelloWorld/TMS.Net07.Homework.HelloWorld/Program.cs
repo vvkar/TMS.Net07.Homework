42c21b000e1516c80f610f826a42f26375fd630f
vvkar <vv.karankevich@gmail.com>
2021-01-27 12:42:56 +0300

    easy task

---
+++ TMS.Net07.Homework.HelloWorld/TMS.Net07.Homework.HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Net07.Homework.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:") ;
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+            Console.WriteLine($"Введенное число - {number}");
+
+            Console.ReadKey();
+        }
+    }
+}
