f99109ba2a8aedbe250b4efb6fb22ceaeeae83ac
vvkar <v.v.karankevich@gmail.com>
2021-01-31 19:00:23 +0300

    medium task

--- TMS.Net07.Homework.DaysOfWeek/TMS.Net07.Homework.DaysOfWeek/Program.cs
+++ TMS.Net07.Homework.DaysOfWeek/TMS.Net07.Homework.DaysOfWeek/Program.cs
@@ -8,92 +8,21 @@ namespace TMS.Net07.Homework.DaysOfWeek
 {
     class Program
     {
-        enum DaysOfWeek
-        {
-            Monday = 1,
-            Tuesday,
-            Wednesday,
-            Thursday,
-            Friday,
-            Saturday,
-            Sunday
-        }
         static void Main(string[] args)
         {
-            DaysOfWeek day;
-            Console.Write("Input day of week name you wish to translate: ");
+           Console.Write("Please input date: ");
             do
             {
-                bool check = false;
-                do
-                {
-                    string dayName = Console.ReadLine();
-                    if (dayName == "exit")
-                        return;
+                string input = Console.ReadLine();
 
-                    switch (dayName)
-                    {
-                        case "Monday":
-                            day = DaysOfWeek.Monday;
-                            check = true;
-                            break;
-                        case "Tuesday":
-                            day = DaysOfWeek.Tuesday;
-                            check = true;
-                            break;
-                        case "Wednesday":
-                            day = DaysOfWeek.Wednesday;
-                            check = true;
-                            break;
-                        case "thursday":
-                            day = DaysOfWeek.Thursday;
-                            check = true;
-                            break;
-                        case "Friday":
-                            day = DaysOfWeek.Friday;
-                            check = true;
-                            break;
-                        case "Saturday":
-                            day = DaysOfWeek.Saturday;
-                            check = true;
-                            break;
-                        case "Sunday":
-                            day = DaysOfWeek.Sunday;
-                            check = true;
-                            break;
-                        default:
-                            Console.WriteLine("Invalid input, try again.");
-                            day = 0;
-                            break;
-                    }
-                }
-                while (check == false);
+                if (input == "exit")
+                    return;
 
-                switch (day)
-                {
-                    case DaysOfWeek.Monday:
-                        Console.WriteLine("Translate: Понедельник");
-                        break;
-                    case DaysOfWeek.Tuesday:
-                        Console.WriteLine("Translate: Вторник");
-                        break;
-                    case DaysOfWeek.Wednesday:
-                        Console.WriteLine("Translate: Среда");
-                        break;
-                    case DaysOfWeek.Thursday:
-                        Console.WriteLine("Translate: Четверг");
-                        break;
-                    case DaysOfWeek.Friday:
-                        Console.WriteLine("Translate: Пятница");
-                        break;
-                    case DaysOfWeek.Saturday:
-                        Console.WriteLine("Translate: Суббота");
-                        break;
-                    case DaysOfWeek.Sunday:
-                        Console.WriteLine("Translate: Воскресенье");
-                        break;
-                }
-                Console.Write("Try another day or input 'exit' to close the program: ");
+                var date = Convert.ToDateTime(input);
+
+                Console.WriteLine("Day of week: "+date.DayOfWeek);
+                Console.WriteLine();
+                Console.Write("Try another date or input 'exit' to close the program: ");            
             }
             while (true);
         }
