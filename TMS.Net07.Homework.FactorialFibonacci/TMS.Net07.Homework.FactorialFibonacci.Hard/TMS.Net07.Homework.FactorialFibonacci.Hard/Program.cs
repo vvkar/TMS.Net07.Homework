875c69c99310358d10ac12074f43b6d7ec07eba5
vvkar <v.v.karankevich@gmail.com>
2021-02-09 00:55:43 +0300

    hard task

---
+++ TMS.Net07.Homework.FactorialFibonacci/TMS.Net07.Homework.FactorialFibonacci.Hard/TMS.Net07.Homework.FactorialFibonacci.Hard/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Net07.Homework.FactorialFibonacci.Hard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Fibonacci(n));
+
+            Console.ReadKey();
+        }
+
+        static int Fibonacci(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }                
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
