1db1ffccf6a08ff334f7a3efb61408d62960e6ff
vvkar <v.v.karankevich@gmail.com>
2021-02-09 00:21:20 +0300

    easy task

---
+++ TMS.Net07.Homework.FactorialFibonacci/TMS.Net07.Homework.FactorialFibonacci.Easy/TMS.Net07.Homework.FactorialFibonacci.Easy/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Net07.Homework.FactorialFibonacci.Easy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            int factorial = n;
+
+            for (int i = n-1; i > 0; i--)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine(factorial);
+
+            Console.ReadKey();
+        }
+    }
+}
