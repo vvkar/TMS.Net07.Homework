41d0da6cdc28b30997e085022b2da1c0e2daebf0
vvkar <v.v.karankevich@gmail.com>
2021-02-09 00:35:37 +0300

    medium task

---
+++ TMS.Net07.Homework.FactorialFibonacci/TMS.Net07.Homework.FactorialFibonacci.Medium/TMS.Net07.Homework.FactorialFibonacci.Medium/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Net07.Homework.FactorialFibonacci.Medium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetFactorial(n));
+
+            Console.ReadKey();
+        }
+
+        static int GetFactorial(int n)
+        {
+            if (n==1)
+            {
+                return n;
+            }
+            return n * GetFactorial(n - 1);
+        }
+    }
+}
