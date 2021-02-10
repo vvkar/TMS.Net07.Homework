f1914e6ff83e0320ea6021026da08d39ecf3127b
vvkar <v.v.karankevich@gmail.com>
2021-02-10 16:42:42 +0300

    fibonacci effective method

---
+++ TMS.Net07.Homework.Task5/TMS.Net07.Homework.Task5.Fibonacci/TMS.Net07.Homework.Task5.Fibonacci/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Net07.Homework.Task5.Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var buffer = new int[n+1];
+
+            var res = Fibonacci(n, buffer); //calculating the N-th Fibonacci element
+
+            Console.WriteLine(res);            
+
+            Console.ReadKey();
+
+        }
+
+        public static int Fibonacci(int n, int [] buffer)   //why static?
+        {
+            if (n==0 || n==1)   //when recursion reaches first or zero element 
+            {
+                buffer[n] = n;  //first or zero element becomes 0 or 1 
+                return n;       //ending of the recursion
+            }
+            if (buffer [n]==0)  //check if the N-th element is already written
+            {
+                buffer[n] = Fibonacci(n - 2, buffer) + Fibonacci(n - 1, buffer);    //recursion
+            }
+            return buffer[n];   //if the N-th element in buffer[n] already exist 
+        }
+    }
+}
